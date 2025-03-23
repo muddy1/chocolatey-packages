8b775c0765ce596401a32d13f94876d8f7405fd4
muddy1 <c0wf3d@gmail.com>
2025-03-23 16:25:00 -0400

    prep for system maintenance

---
+++ vortex/update.ps1
@@ -0,0 +1,25 @@
+﻿Import-Module Chocolatey-AU
+
+function global:au_GetLatest {
+    $LatestRelease = Invoke-RestMethod -UseBasicParsing -Uri "https://api.github.com/repos/Nexus-Mods/vortex/releases/latest"
+
+    @{
+        URL64        = ($LatestRelease.assets | Where-Object {$_.name.EndsWith("_windows_x86_64.msi")}).browser_download_url
+        Version      = $LatestRelease.tag_name.Replace('elk-native-v', '')
+        ReleaseNotes = $LatestRelease.html_url
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*(\$)url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+        }
+
+        ".nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`$1$($Latest.ReleaseNotes)`$2"
+        }
+    }
+}
