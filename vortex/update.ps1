5c1d507a9aab411afddbfede99f05754bd611865
muddy1 <c0wf3d@gmail.com>
2025-03-27 22:05:03 -0400

    update script prep

--- vortex/update.ps1
+++ vortex/update.ps1
@@ -1,25 +1,45 @@
-﻿Import-Module Chocolatey-AU
+﻿#Requires -Version 5.0
+#Requires -Modules chocolatey-au
+[cmdletbinding()]
+param (
+  [switch]$Force
+)
 
-function global:au_GetLatest {
-    $LatestRelease = Invoke-RestMethod -UseBasicParsing -Uri "https://api.github.com/repos/Nexus-Mods/vortex/releases/latest"
+$ErrorActionPreference = 'Stop'
 
-    @{
-        URL64        = ($LatestRelease.assets | Where-Object {$_.name.EndsWith("_windows_x86_64.msi")}).browser_download_url
-        Version      = $LatestRelease.tag_name.Replace('elk-native-v', '')
-        ReleaseNotes = $LatestRelease.html_url
-    }
+$downloadUrl = 'https://api.github.com/repos/Nexus-Mods/vortex/releases/latest'
+
+function Get-RemoteFileVersion( [string] $Url ) {
+  $fn = [System.IO.Path]::GetTempFileName()
+  Invoke-WebRequest $Url -Outfile $fn -UseBasicParsing
+  $res = (Get-ItemProperty $fn).VersionInfo.FileVersion
+  Remove-Item $fn -ea ignore
+  return $res
+}
+
+function global:au_BeforeUpdate {
+  $Latest.Checksum32 = Get-RemoteChecksum -Url $Latest.Url32 -Algorithm 'SHA256'
 }
 
 function global:au_SearchReplace {
-    @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*(\$)url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
-            "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
-        }
-
-        ".nuspec" = @{
-            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`$1$($Latest.ReleaseNotes)`$2"
-        }
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^[$]checksum\s*=\s*)'.*'"   = "`${1}'$($Latest.Checksum32)'"
     }
+  }
+}
+
+function global:au_GetLatest {
+  $version = Get-RemoteFileVersion -Url $downloadUrl
+
+  return @{
+    Version = $version
+    URL32   = $downloadUrl
+  }
+}
+
+# run the update only if this script is not sourced by the virtual package
+if ($MyInvocation.InvocationName -ne '.') {
+  [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+  Update-Package -NoCheckUrl -NoCheckChocoVersion -NoReadme -ChecksumFor none -Force:$Force
 }
