80eed3331b1e83d1fe7670ba062432d149d3dbcf
muddy1 <c0wf3d@gmail.com>
2025-06-12 01:14:18 -0400

    Update chocolateyinstall.ps1

--- stable/vroid-studio/tools/chocolateyinstall.ps1
+++ stable/vroid-studio/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.vroid.com/dist/WIQ8Wy7INR/VRoidStudio-v2.1.5-win.exe'
+$url        = 'https://download.vroid.com/dist/fowv03gyNN/VRoidStudio-v2.1.6-win.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'vroid-studio*'
 
-  checksum      = 'B576E3E34CDEED377CDC3249780A6EBA002A769B20814CBD2A925B3705EFB568'
+  checksum      = '831E151BCB9C0FF52E760A0D6D005974A80E9A6E9326A0397E91A359A2C7471E'
   checksumType  = 'sha256'
 
   silentArgs    = "/SP- /VERYSILENT"
