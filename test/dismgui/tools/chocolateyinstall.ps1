d7b3acf47ebe019f50c579c60033b1beb453ee8e
muddy1 <c0wf3d@gmail.com>
2025-06-13 09:04:49 -0400

    Update chocolateyinstall.ps1

--- test/dismgui/tools/chocolateyinstall.ps1
+++ test/dismgui/tools/chocolateyinstall.ps1
@@ -1,25 +1,24 @@
 ﻿$ErrorActionPreference = 'Stop';
-
-$packageName = 'dismgui'
-$url         = 'https://github.com/mikecel79/DISMGUI/releases/download/4.0/DISM.GUI.zip'
-$checksum    = '0DA4B2F08424CECA3EA1F4996566366B27C9B5A6E1C0EF37E89BA1224BF91041'
-$installDir  = '{0}\dismgui' -f $Env:SystemDrive
+$toolsDir 			   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$pp                    = Get-PackageParameters
+$shortcutName          = 'dismgui.lnk'
+$url                   = 'https://github.com/mikecel79/DISMGUI/releases/download/4.0/DISM.GUI.zip'
 
 $packageArgs = @{
-  packageName    = $packageName
-  url            = $url
-  checksum       = $checksum
-  checksumType   = 'sha256'
-  unzipLocation  = $installDir
+  Url           = $url
+  Checksum      = '0DA4B2F08424CECA3EA1F4996566366B27C9B5A6E1C0EF37E89BA1224BF91041'
+  ChecksumType  = 'sha256'
+  UnzipLocation = $toolsDir
+  PackageName   = 'dismgui'
 }
 
 Install-ChocolateyZipPackage @packageArgs
 
-
-
-
-
-
+if ($pp['desktopicon']) {
+	$desktopicon = (Join-Path ([Environment]::GetFolderPath("Desktop")) $shortcutName)
+	Write-Host -ForegroundColor green 'Adding ' $desktopicon
+	Install-ChocolateyShortcut -ShortcutFilePath $desktopicon -TargetPath $exepath  -RunAsAdmin
+}
 
 
 
