206ce8317af716f8cd305e2d3a28dc5c17db4c17
muddy1 <c0wf3d@gmail.com>
2025-06-13 15:13:40 -0400

    update

---
+++ test/flakiesadb/tools/chocolateyuninstall.ps1
@@ -0,0 +1,33 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'flakiesadbgui'
+$url         = 'https://flakie.co.uk/downloads/FlakieADBGUI.zip'
+$checksum    = 'CCB72D93BF7FD582E9AC10A43692E83E3B2D4B1B5495CD0D4EF2E9A36B212975'
+$installDir  = '{0}\flakiesadb\' -f $Env:SystemDrive
+
+$packageArgs = @{
+  packageName    = $packageName
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  unzipLocation  = $installDir
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
