2b3af24b919c85d4bccf1aec34dbf4759f41cbb5
muddy1 <c0wf3d@gmail.com>
2026-01-04 02:06:25 -0500

    update

--- test/jdownloader/tools/chocolateyinstall.ps1
+++ test/jdownloader/tools/chocolateyinstall.ps1
@@ -1,24 +1,26 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = ''
+$fileLocation = Join-Path $toolsDir 'JDownloader2Setup_windows-amd64_v21_0_7.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $url
+  file         = $fileLocation
 
-  softwareName  = '*'
+  softwareName  = 'jdownloader*'
 
-  checksum      = ''
+  checksum      = '504B6AF59EA6C42582AFDCF48F3CB8165CD92DAACECAC9FB01FFF67054617E45'
   checksumType  = 'sha256'
 
-  silentArgs    = ""
+  silentArgs    = "-q"
   validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
 
+# Remove the installers as there is no more need for it
+Remove-Item $toolsDir\*.exe -ea 0 -Force
 
 
 
