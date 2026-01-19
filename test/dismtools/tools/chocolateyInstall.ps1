6d7607f881ab2a68771760543cbc1ebb108be458
muddy1 <c0wf3d@gmail.com>
2026-01-18 22:25:53 -0500

    dismtoolfirstcommit

---
+++ test/dismtools/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/Nexus-Mods/Vortex/releases/download/v1.14.1/vortex-setup-1.14.1.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'vortex*'
+
+  checksum      = '09BD2449591CFBB2F599815A68C8C6A32D09111D76FA409F0975267B9C5930FE'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/S"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
