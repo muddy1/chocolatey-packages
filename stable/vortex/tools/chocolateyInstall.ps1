233a4dbf3c341f111b1321d09196f7821f701cc8
muddy1 <c0wf3d@gmail.com>
2026-02-10 06:10:46 -0500

    update

--- stable/vortex/tools/chocolateyInstall.ps1
+++ stable/vortex/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/Nexus-Mods/Vortex/releases/download/v1.15.2/vortex-setup-1.15.2.exe'
+$url        = 'https://github.com/Nexus-Mods/Vortex/releases/download/v1.16.1/vortex-setup-1.16.1.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'vortex*'
 
-  checksum      = 'E321B871E940CEAC507B58E0C49AE407C3BE41D4F4E01A8DCA09133CC5D3FB7B'
+  checksum      = 'BAE1A465E8C82BA71999E96AB05B68736A4381800111607D2F59C13BD8991E82'
   checksumType  = 'sha256'
 
   silentArgs    = "/S"
