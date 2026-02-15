6b252e9997ae1a2ce7b49a0bb106e0342cc91480
muddy1 <c0wf3d@gmail.com>
2026-02-15 03:40:42 -0500

    update

--- stable/dismtools.prerelease/tools/chocolateyInstall.ps1
+++ stable/dismtools.prerelease/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/CodingWonders/DISMTools/releases/download/v0.7.3_pre_2621/dt_setup.exe'
+$url        = 'https://github.com/CodingWonders/DISMTools/releases/download/v0.7.3_pre_2622/dt_setup.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'dismtools*'
 
-  checksum      = '89D9038487097176EA6E11A94BC3B35FFD3B77C63061F18A448BD666F81AD037'
+  checksum      = '69D67515AB67D42A99A4D5B0DAEE6925EA2963E0618E936A4187EBC345A68041'
   checksumType  = 'sha256'
 
   silentArgs    = "/SP- /VERYSILENT"
